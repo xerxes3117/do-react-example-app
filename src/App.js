1e835a60e6484104c8b47bd2f3e764aa8121dbbd
xerxes3117 <vsnsit@gmail.com>
2020-02-13 02:31:38 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Digital ocean droplet example1</h1>
+      <h1>Digital ocean droplet example3</h1>
     </div>
   );
 }
